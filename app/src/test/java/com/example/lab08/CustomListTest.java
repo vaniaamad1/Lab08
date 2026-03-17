c3c97050aa0b37244465706dfe5994eadddd9f9a
vaniaamad1 <27100318@lums.edu.pk>
2026-03-18 01:55:33 +0500

    countCities test failing

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -22,4 +22,12 @@ public class CustomListTest {
         assertFalse(list.hasCity(calgary));
     }
 
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        list.addCity(new City("Calgary", "AB"));
+        list.addCity(new City("Edmonton", "AB"));
+        assertEquals(2, list.countCities());
+    }
+
 }
